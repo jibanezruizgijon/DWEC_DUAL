80b800949af8bbe850237f4924bd885d89265e3b
Jonathan <jonathan.ibanez-pinero@iesruizgijon.com>
2026-03-10 22:31:19 +0100

    repaso_formulario

--- estudio_angular/src/app/formulario3/formulario3.component.ts
+++ estudio_angular/src/app/formulario3/formulario3.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario3',
@@ -9,6 +9,20 @@ import { FormControl } from '@angular/forms';
 })
 export class Formulario3Component {
 
-  name = new FormControl("");
-  email = new FormControl("");
+  get name() {return this.formUser.get('name') as FormControl;
+  }
+  get email() {return this.formUser.get('email') as FormControl;
+  }
+
+  formUser = new FormGroup({
+    'name': new FormControl("", Validators.required),
+    'email': new FormControl("",[ Validators.required, Validators.email])
+  });
+
+   procesar() {
+    console.log(this.formUser.value);
+  }
+
+  // name = new FormControl("", Validators.required);
+  // email = new FormControl("",[ Validators.required, Validators.email]);
 }
