54744453e12613635b5eee6aff715339b4c6fb83
Jonathan <jonathan.ibanez-pinero@iesruizgijon.com>
2026-03-11 14:17:21 +0100

    repaso_formulario2

--- estudio_angular/src/app/app.module.ts
+++ estudio_angular/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { SwitchComponent } from './switch/switch.component';
 import { Formulario2Component } from './formulario2/formulario2.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Formulario3Component } from './formulario3/formulario3.component';
+import { ChildrenComponent } from './children/children.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { Formulario3Component } from './formulario3/formulario3.component';
     BuclesComponent,
     SwitchComponent,
     Formulario2Component,
-    Formulario3Component
+    Formulario3Component,
+    ChildrenComponent
   ],
   imports: [
     BrowserModule,
--- estudio_angular/src/app/formulario3/formulario3.component.ts
+++ estudio_angular/src/app/formulario3/formulario3.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario3',
@@ -9,14 +9,21 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class Formulario3Component {
 
+  private fb = inject(FormBuilder);
+
   get name() {return this.formUser.get('name') as FormControl;
   }
   get email() {return this.formUser.get('email') as FormControl;
   }
 
-  formUser = new FormGroup({
-    'name': new FormControl("", Validators.required),
-    'email': new FormControl("",[ Validators.required, Validators.email])
+  // formUser = new FormGroup({
+  //   'name': new FormControl("", Validators.required),
+  //   'email': new FormControl("",[ Validators.required, Validators.email])
+  // });
+
+  formUser = this.fb.group({
+    'name':["", Validators.required],
+    'email':["",[ Validators.required, Validators.email]]
   });
 
    procesar() {
