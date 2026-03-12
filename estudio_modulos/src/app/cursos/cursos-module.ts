39011b5554028d33e6938044c1b55b8957dd4382
Jonathan <jonathan.ibanez-pinero@iesruizgijon.com>
2026-03-12 14:52:07 +0100

    repaso_modulos

--- estudio_modulos/src/app/cursos/cursos-module.ts
+++ estudio_modulos/src/app/cursos/cursos-module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CursosRoutingModule } from './cursos-routing-module';
-import { ListaCursos } from './lista-cursos/lista-cursos';
+import { ListaCursos } from './pages/lista-cursos/lista-cursos';
 
 @NgModule({
   declarations: [ListaCursos],
