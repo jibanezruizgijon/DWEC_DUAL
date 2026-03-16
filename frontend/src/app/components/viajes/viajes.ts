3df554e2344a6fbedb8cd91a22c19262137974db
Jonathan <jonathan.ibanez-pinero@iesruizgijon.com>
2026-03-16 22:11:09 +0100

    componentes_viajes

--- frontend/src/app/components/viajes/viajes.ts
+++ frontend/src/app/components/viajes/viajes.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { ViajesService } from '../../services/viajes.service';
+import { Viaje } from '../../interfaces/viaje';
 
 @Component({
   selector: 'app-viajes',
@@ -7,11 +8,29 @@ import { ViajesService } from '../../services/viajes.service';
   templateUrl: './viajes.html',
   styleUrl: './viajes.scss',
 })
-export class Viajes {
+export class Viajes implements OnInit{
 
   private viajesService = inject(ViajesService);
+  private cdr = inject(ChangeDetectorRef);
 
-  viajes$ = this.viajesService.obtenerViajes();
+  listaViajes: Viaje[] = [];
+  mensajeError = '';
+  cargando = true;
   
+  ngOnInit(): void {
+    this.viajesService.obtenerViajes().subscribe({
+      next: (viajes) => {
+        this.listaViajes = viajes;
+        this.cargando = false;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+       this.cargando = false;
+        this.mensajeError = 'No se han podido cargar tus viajes.';
+        console.error(err);
+        this.cdr.detectChanges();
+      }
+    })
+  }
 
 }
--- frontend/src/app/interfaces/viaje.ts
+++ frontend/src/app/interfaces/viaje.ts
@@ -2,4 +2,8 @@ export interface Viaje {
     id: number;
     lugar_id: number;
     fecha_viaje:string;
+    lugar?: {
+        id: number;
+        nombre: string;
+    };
 }
